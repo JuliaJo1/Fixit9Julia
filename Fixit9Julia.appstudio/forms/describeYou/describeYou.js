0f883d4074caafe358c0256c7f9c22e6c74b1bb2
JuliaJo1 <Jlj26718@creighton.edu>
2020-10-25 15:31:30 -0500

    fixit9

---
+++ Fixit9Julia.appstudio/forms/describeYou/describeYou.js
@@ -0,0 +1,11 @@
+radDescribeYou.onchange=function(){
+  lblForm.value = `I agree you are a very ${$("input[name=radDescribeYou]:checked").prop("value")} person!`
+}
+
+btnNextTwo.onclick=function(){
+  ChangeForm(favExercises)
+}
+
+
+
+
